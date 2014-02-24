1b1180cd1f612154cb61f16e2655efbe92648ec1
Alastair Surin <alastairjsurin@gmail.com>
2014-02-23 23:50:40 -0500

    Accessibility tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @slide_alt_texts =
+        {
+            1 => 'Alastair Surin and Patricia Johnson on the pier',
+            2 => 'Alastair Surin and Patricia Johnson on a bench',
+            3 => 'Alastair Surin and Patricia Johnson viewed from over the bay',
+            4 => 'Alastair Surin and Patricia Johnson posing outside Pat Troy\'s',
+            5 => 'Alastair Surin and Patricia Johnson hugging outside Pat Troy\'s',
+            6 => 'Alastair Surin and Patricia Johnson in the park'
+        }
   end
 end
